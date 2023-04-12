25e02f0c2f22eaaa84b36f829c9af17b29f0acfa
Muhammed Shittu <devshittu@gmail.com>
2023-04-12 23:37:04 +0100

    Implemented the search queryset.

--- books/views.py
+++ books/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import Book
 
-class BookListView(ListView):
+
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "books/book_list.html"
-    
-class BookDetailView(DetailView):
+    login_url = "account_login"
+
+
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = "book"
     template_name = "books/book_detail.html"
+    login_url = "account_login"
+    permission_required = "book.special_status"
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+    queryset = Book.objects.filter(title__icontains="beginners")
+
+    def get_queryset(self):
+        query = self,request.GET.get("q")
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(title__icontains=query)
+        )
